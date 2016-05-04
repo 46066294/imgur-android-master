401e053bde99f2899c70dfd499a111fde2ee07d2
46066294 <46066294p@iespoblenou.org>
2016-05-04 23:33:12 +0200

    API funcional per a pujar imatges a imgur Introduir key's a Constants.java

---
+++ app/src/main/java/akiniyalocts/imgurapiexample/imgurmodel/Upload.java
@@ -0,0 +1,15 @@
+package akiniyalocts.imgurapiexample.imgurmodel;
+
+import java.io.File;
+
+/**
+ *
+ *
+ * Basic object for upload.
+ */
+public class Upload {
+  public File image;
+  public String title;
+  public String description;
+  public String albumId;
+}
